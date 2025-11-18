ed04a27c481f84a1c012eea63bef584eaf06dd5d
gr4nt-json <famadorgrant@gmail.com>
2025-11-18 10:18:41 +0400

    Add files via upload

---
+++ Exercise 07 - Some Counting/07-Some Counting.py
@@ -0,0 +1,29 @@
+#Counts from 0 to 50
+zerofifty = 0
+for x in range(51):
+    print(zerofifty)
+    zerofifty = zerofifty + 1
+
+#Counts from 50 to 0
+fiftyzero = 50
+for x in range(51):
+    print(fiftyzero)
+    fiftyzero = fiftyzero - 1
+
+#Counts from 30 to 50
+thirtyfifty = 30
+for x in range(21):
+    print(thirtyfifty)
+    thirtyfifty = thirtyfifty + 1
+
+#Counts from 50 to 10 by 2 
+fiftyten = 50
+for x in range(21):
+    print(fiftyten)
+    fiftyten = fiftyten - 2
+
+#Counts frm 100 to 200 by 5
+onehtwoh = 100
+for x in range(21):
+    print(onehtwoh)
+    onehtwoh = onehtwoh + 5
